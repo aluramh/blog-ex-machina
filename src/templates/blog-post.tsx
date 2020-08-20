83cbb8be9bf75845875814eeca399a35f02a6857
Alex R <aluramh@gmail.com>
2020-08-20 12:54:51 -0700

    using new layout

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -4,6 +4,8 @@ import { Helmet } from "react-helmet";
 import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
+import { rhythm, scale } from "../utils/typography";
+import SEO from "../components/seo";
 
 interface BlogPostTemplateProps {
   content: any;
@@ -62,33 +64,157 @@ interface BlogPostProps {
         description: string;
         title: string;
         tags: string[];
+        // ???
+        date;
       };
     };
   };
+  pageContext: { next: any; previous: any };
+  location: any;
 }
 
-const BlogPost: FC<BlogPostProps> = ({ data }) => {
+interface BlogPostLayoutProps {
+  location: { pathname: string };
+  title: string;
+}
+
+const BlogPostLayout: FC<BlogPostLayoutProps> = ({
+  location,
+  title,
+  children,
+}) => {
+  const rootPath = `lolazo`; //`${__PATH_PREFIX__}/`;
+  let header;
+
+  if (location.pathname === rootPath) {
+    header = (
+      <h1
+        style={{
+          ...scale(1.5),
+          marginBottom: rhythm(1.5),
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h1>
+    );
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+    );
+  }
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+    >
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
+  );
+};
+
+const BlogPost: FC<BlogPostProps> = ({ data, pageContext, location }) => {
   const { markdownRemark: post } = data;
+  const { previous, next } = pageContext;
 
   return (
-    <Layout>
-      <BlogPostTemplate
-        content={post.html}
-        contentComponent={HTMLContent}
-        description={post.frontmatter.description}
-        helmet={
-          <Helmet titleTemplate="%s | Blog">
-            <title>{`${post.frontmatter.title}`}</title>
-            <meta
-              name="description"
-              content={`${post.frontmatter.description}`}
-            />
-          </Helmet>
-        }
-        tags={post.frontmatter.tags}
+    <BlogPostLayout location={location} title="SAMPLE TITELE">
+      <SEO
         title={post.frontmatter.title}
+        description={
+          post.frontmatter.description
+          // || post?.excerpt
+        }
       />
-    </Layout>
+      <article>
+        <header>
+          <h1
+            style={{
+              marginTop: rhythm(1),
+              marginBottom: 0,
+            }}
+          >
+            {post.frontmatter.title}
+          </h1>
+          <p
+            style={{
+              ...scale(-1 / 5),
+              display: `block`,
+              marginBottom: rhythm(1),
+            }}
+          >
+            {post.frontmatter?.date}
+          </p>
+        </header>
+        <section dangerouslySetInnerHTML={{ __html: post.html }} />
+        <hr
+          style={{
+            marginBottom: rhythm(1),
+          }}
+        />
+        <footer>{/* <Bio /> */}</footer>
+      </article>
+
+      <nav>
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </BlogPostLayout>
   );
 };
 
