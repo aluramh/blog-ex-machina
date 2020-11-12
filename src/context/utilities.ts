22003d0f59c962d9170b2b6cab2a90d8bf55901d
Alex Ramirez <alexramirez@LiveObjects-Macbook.local>
2020-11-11 22:08:19 -0800

    util

--- src/context/utilities.ts
+++ src/context/utilities.ts
@@ -21,3 +21,14 @@ export function getCookie(cookieName: string): any {
   }
   return "";
 }
+
+/**
+ * "Cheap" way to turn on dark mode
+ */
+export function comeToTheDarkside() {
+  document.documentElement.style.filter = "invert(1) hue-rotate(180deg)";
+  document.body
+    .querySelectorAll("img, picture, video")
+    // @ts-ignore
+    .forEach((el) => (el.style.filter = "invert(1) hue-rotate(180deg)"));
+}
