b3e56ca21901c27223f58e0b446824c8346dc6e8
Alex Ramirez <alexramirez@LiveObjects-Macbook.local>
2020-11-12 20:48:05 -0800

    Home icon always. Switched to localstorage instead of cookies.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,10 +18,11 @@ const Navbar: FC<Props> = (props) => {
   const { theme, toggleTheme, bodyBackgroundClass } = useTheme();
   const { className = "" } = props;
 
-  const iconColor = theme === "light" ? "black" : "white";
+  const iconColor = theme === "light" ? "black" : "#cbd5e0";
   const navbarLinkClass = theme === "light" ? "text-gray-700" : "text-gray-300";
   const navbarBorder =
     theme === "light" ? "border-gray-500" : "border-gray-700 ";
+  const currentlyInHome = !(window.location.pathname !== "/");
 
   return (
     <nav
@@ -36,15 +37,13 @@ const Navbar: FC<Props> = (props) => {
           <div className="flex flex-row">
             {/* Brand */}
             <div className={`${navbarLinkClass} navbar-brand mr-3`}>
-              {window.location.pathname !== "/" && (
-                <CustomLink to="/" title="Logo">
-                  <HomeIcon
-                    color={iconColor}
-                    size="40px"
-                    style={{ transform: "translateY(3px)" }}
-                  />
-                </CustomLink>
-              )}
+              <CustomLink to="/" title="Logo">
+                <HomeIcon
+                  color={iconColor}
+                  size="40px"
+                  style={{ transform: "translateY(3px)" }}
+                />
+              </CustomLink>
             </div>
 
             {/* Menu items */}
