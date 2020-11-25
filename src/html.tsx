e3881cf394430d6791e1fc69a9d8151b85235518
Alex Ramirez <alexramirez@LiveObjects-Macbook.local>
2020-11-25 11:23:03 -0800

    more explicit theme switching

--- src/html.tsx
+++ src/html.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const themeLoadScript: string = `
 // On page load or when changing themes, best to add inline in \`head\` to avoid FOUC
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.querySelector('html').classList.add('dark')
+  document.querySelector('html').className = 'dark'
 } else {
-  document.querySelector('html').classList.remove('dark')
+  document.querySelector('html').className = 'light'
 }
 `;
 
